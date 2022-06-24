578d84d25bfb0415e9afd1fe4457c8bda2fd4506
Alex Khomenko <oleksandr.khomenko2000@gmail.com>
2022-06-24 15:47:10 +0200

    Create exchange for accounts

--- apps/accounts/src/app/auth/auth.controller.ts
+++ apps/accounts/src/app/auth/auth.controller.ts
@@ -1,19 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AccountsRegister, AccountsLogin } from '@microservices/contracts';
+import { RMQRoute } from 'nestjs-rmq';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/register')
+  @RMQRoute(AccountsRegister.topic)
   async register(
     dto: AccountsRegister.Request
   ): Promise<AccountsRegister.Response> {
     return this.authService.register(dto);
   }
 
-  @Post('/login')
+  @RMQRoute(AccountsLogin.topic)
   async login(
     @Body() { email, password }: AccountsLogin.Request
   ): Promise<AccountsLogin.Response> {
---
+++ apps/accounts/src/app/configs/rmq.config.ts
@@ -0,0 +1,22 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import type { IRMQServiceAsyncOptions, IRMQServiceOptions } from 'nestjs-rmq';
+
+export const getRMQConfig = (): IRMQServiceAsyncOptions => {
+  return {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      exchangeName: configService.get('AMQP_EXCHANGE') ?? '',
+      connections: [
+        {
+          login: configService.get('AMQP_USER') ?? '',
+          password: configService.get('AMQP_PASSWORD') ?? '',
+          host: configService.get('AMQP_HOSTNAME') ?? '',
+        },
+      ],
+      queueName: configService.get('AMQP_QUEUE'),
+      prefetchCount: 32,
+      serviceName: 'purple-account',
+    }),
+  };
+};
